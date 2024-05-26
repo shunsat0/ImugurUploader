ed9ca13e2e24abc25738f33c5988f24f4a08df29
youten410 <techregisteremail@gmail.com>
2024-05-26 21:55:50 +0900

    update : #1 Alamofireを使用した書き方に変更。

---
+++ ImgurUploader/View/InfoView.swift
@@ -0,0 +1,31 @@
+//
+//  InfoView.swift
+//  ImgurUploader
+//
+//  Created by Shun Sato on 2024/01/08.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    var body: some View {
+        TabView {
+            ForEach(1...9, id: \.self) { item in
+                Image("\(item)")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.black, lineWidth: 0.5)
+                    )
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+#Preview {
+    InfoView()
+}
