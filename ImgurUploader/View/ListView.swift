ea36b8824855c46d60ba846cb6b045540901892f
youten410 <techregisteremail@gmail.com>
2024-06-01 22:52:17 +0900

    delete : sectionの削除

--- ImgurUploader/View/ListView.swift
+++ ImgurUploader/View/ListView.swift
@@ -10,58 +10,58 @@ import SwiftData
 
 struct ListView: View {
     @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var modelContext
     @Query private var images: [ImageData]
     
     var body: some View {
-        VStack {
-            List {
-                Section(header: Text("Uploaded Images")) {
-                    ForEach(images, id: \.self) { image in
-                        let imageUrl = image.url
-                        
-                        HStack {
-                            if let imageUrl = URL(string: imageUrl) {
-                                    AsyncImage(url: imageUrl) { phase in
-                                        switch phase {
-                                        case .success(let image):
-                                            image.resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                                .frame(width: 60, height: 60)
-                                        case .failure:
-                                            Image(systemName: "photo")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                                .frame(width: 60, height: 60)
-                                        case .empty:
-                                            ProgressView()
-                                        @unknown default:
-                                            EmptyView()
-                                        }
-                                    }
-                                    .frame(width: 100, height: 100)
-                                    .cornerRadius(10)
-                            }
-                            
-                            VStack(alignment: .leading) {
-                                Text(image.url)
-                                    .textSelection(.enabled)
-                                    .font(.caption)
-                                    .foregroundColor(.blue)
-                                
-                                HStack {
-                                    Text("(Delete Code)")
-                                    
-                                    Text(image.deletehas)
-                                        .textSelection(.enabled)
-                                }
-                                .font(.caption2)
-                                .foregroundColor(.gray)
+        List {
+            ForEach(images, id: \.self) { image in
+                let imageUrl = image.url
+                
+                HStack {
+                    if let imageUrl = URL(string: imageUrl) {
+                        AsyncImage(url: imageUrl) { phase in
+                            switch phase {
+                            case .success(let image):
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 60, height: 60)
+                            case .failure:
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 60, height: 60)
+                            case .empty:
+                                ProgressView()
+                            @unknown default:
+                                EmptyView()
                             }
                         }
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(10)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text(image.url)
+                            .textSelection(.enabled)
+                            .font(.caption)
+                            .foregroundColor(.blue)
+                                }
+                            }
+                        
+                        HStack {
+                            Text("(Delete Code)")
+                            
+                            Text(image.deletehas)
+                                .textSelection(.enabled)
+                        }
+                        .font(.caption2)
+                        .foregroundColor(.gray)
                     }
                 }
             }
         }
+        
     }
     
 }
