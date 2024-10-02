530f1de4830aab57a068638f5cb8601fd528ee54
shunsat0 <ellegardenno8@gmail.com>
2024-10-02 23:00:48 +0900

    add : ATTリクエストの追加

--- ImgurUploader/ImgurUploaderApp.swift
+++ ImgurUploader/ImgurUploaderApp.swift
@@ -9,27 +9,53 @@ import SwiftUI
 import SwiftData
 import GoogleMobileAds
 import Firebase
+import AppTrackingTransparency
+import AdSupport
 
-
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        
         FirebaseApp.configure()
+        
+        // iOS 14以降の場合、ATTリクエストを遅延実行
+        if #available(iOS 14, *) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.requestIDFA()
+            }
+        } else {
+            // iOS 14未満の場合は直接AdMobを初期化
+            self.initializeAdMob()
+        }
         
         return true
     }
+    
+    @available(iOS 14, *)
+    func requestIDFA() {
+        ATTrackingManager.requestTrackingAuthorization { status in
+            DispatchQueue.main.async {
+                switch status {
+                case .authorized, .denied, .restricted, .notDetermined:
+                    // どの状態でもAdMobを初期化します
+                    self.initializeAdMob()
+                @unknown default:
+                    break
+                }
+            }
+        }
+    }
+    
+    func initializeAdMob() {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
 }
-
 
 @main
 struct ImgurUploaderApp: App {
     init() {
         UIPageControl.appearance().currentPageIndicatorTintColor = UIColor.systemBlue
-        
         UIPageControl.appearance().pageIndicatorTintColor = UIColor.systemGray
     }
     
