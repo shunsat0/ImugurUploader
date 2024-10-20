3fd40585605aafc07e9927e2a6bb54e6fbf86144
shunsat0 <ellegardenno8@gmail.com>
2024-10-20 21:29:11 +0900

    Update : 画像を配列で持つように変更

--- ImgurUploader/ViewModel/DropboxViewModel.swift
+++ ImgurUploader/ViewModel/DropboxViewModel.swift
@@ -13,15 +13,16 @@ class DropboxViewModel: ObservableObject {
     @Published var files: [Files.Metadata] = []
     @Published var selectedImage: UIImage? = nil
     @Published var isLoading = false
-
+    @Published var dropboxImages: [String: UIImage] = [:]
+    
     init() {
         checkAuthentication()
     }
-
+    
     func checkAuthentication() {
         isAuthenticated = DropboxClientsManager.authorizedClient != nil
     }
-
+    
     func performLogin() {
         let scopeRequest = ScopeRequest(
             scopeType: .user,
@@ -42,7 +43,7 @@ class DropboxViewModel: ObservableObject {
             scopeRequest: scopeRequest
         )
     }
-
+    
     func handleOpenURL(_ url: URL) {
         let oauthCompletion: DropboxOAuthCompletion = {
             if let authResult = $0 {
@@ -59,32 +60,40 @@ class DropboxViewModel: ObservableObject {
         }
         DropboxClientsManager.handleRedirectURL(url, backgroundSessionIdentifier: "patata", completion: oauthCompletion)
     }
-
+    
     func listFiles() {
         guard let client = DropboxClientsManager.authorizedClient else {
             print("User is not logged in")
             return
         }
-
+        
         isLoading = true
         client.files.listFolder(path: "").response { response, error in
-            self.isLoading = false
-            if let result = response {
-                // 画像ファイルのみをフィルタリング
-                self.files = result.entries.filter { file in
-                    if let fileMetadata = file as? Files.FileMetadata {
-                        let imageExtensions = ["jpg", "jpeg", "png", "gif"]
-                        return imageExtensions.contains((fileMetadata.name as NSString).pathExtension.lowercased())
+            DispatchQueue.main.async {
+                self.isLoading = false
+                if let result = response {
+                    // 画像ファイルのみをフィルタリング
+                    self.files = result.entries.filter { file in
+                        if let fileMetadata = file as? Files.FileMetadata {
+                            let imageExtensions = ["jpg", "jpeg", "png", "gif"]
+                            return imageExtensions.contains((fileMetadata.name as NSString).pathExtension.lowercased())
+                        }
+                        return false
                     }
-                    return false
+                    print("Filtered files: \(self.files)")
+                    
+                    // ファイルリストを取得した後、各画像を自動的にダウンロード
+                    self.files.forEach { file in
+                        self.downloadImage(file)
+                    }
+                } else if let error = error {
+                    print("Error listing files: \(error)")
                 }
-                print("Filtered files: \(self.files)")
-            } else if let error = error {
-                print("Error listing files: \(error)")
             }
         }
     }
 
+    
     func downloadImage(_ file: Files.Metadata) {
         guard let client = DropboxClientsManager.authorizedClient else {
             print("User is not logged in")
@@ -92,13 +101,13 @@ class DropboxViewModel: ObservableObject {
         }
 
         if let fileMetadata = file as? Files.FileMetadata {
+            print("Downloading image for path: \(fileMetadata.pathLower ?? "unknown")")
             client.files.download(path: fileMetadata.pathLower ?? "").response { response, error in
-                if let (_, data) = response {
-                    // ダウンロードしたデータをUIImageに変換して表示用に保存
-                    if let image = UIImage(data: data) {
-                        self.selectedImage = image
-                    } else {
-                        print("Downloaded data is not a valid image")
+                if let (_, data) = response, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        // ファイルのパスをキーにして画像を保存
+                        self.dropboxImages[fileMetadata.pathLower ?? ""] = image
+                        print("Image downloaded and added to dictionary for path: \(fileMetadata.pathLower ?? "unknown")")
                     }
                 } else if let error = error {
                     print("Error downloading file: \(error)")
@@ -106,4 +115,5 @@ class DropboxViewModel: ObservableObject {
             }
         }
     }
+
 }
