f1b2c443689ae9c67835c4037852987619b2b4e4
shunsat0 <ellegardenno8@gmail.com>
2024-10-22 02:00:53 +0900

    Revert "refactor : dropbox画面のUI修正"
    This reverts commit e15bd1a0c6993f532b4b06773ed98f5a335412c5.

--- ImgurUploader/View/ContentView.swift
+++ ImgurUploader/View/ContentView.swift
@@ -18,9 +18,10 @@ struct ContentView: View {
     @StateObject private var viewModel = ImgurDataViewModel()
     @StateObject private var dropboxViewModel = DropboxViewModel()
     @State private var isShowDropboxList:Bool = false
+    
     @Environment(\.modelContext) private var modelContext
+    
     @State private var showAd: Bool = false
-    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         
@@ -153,48 +154,36 @@ struct ContentView: View {
                 let files = dropboxViewModel.files
                 let images = dropboxViewModel.dropboxImages
                 
-                let threeColumnGrid = [
-                    GridItem(.flexible(minimum: 40), spacing: 0),
-                    GridItem(.flexible(minimum: 40), spacing: 0),
-                    GridItem(.flexible(minimum: 40), spacing: 0),
-                ]
+                let gridItemLayout = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
                 
-                NavigationView {
-                    if !files.isEmpty {
-                        ScrollView {
-                            LazyVGrid(columns: threeColumnGrid, spacing: 0) {
-                                ForEach(files, id: \.pathLower) { file in
-                                    if let image = images[file.pathLower ?? ""] {
-                                        Image(uiImage: image)
-                                            .resizable()
-                                            .aspectRatio(1, contentMode: .fill)
-                                            .border(colorScheme == .dark ? .black : .white)
-                                            .onTapGesture {
-                                                self.image = image
-                                                isShowDropboxList = false
-                                                isSelected = true
-                                            }
-                                    } else {
-                                        ProgressView()
-                                            .frame(width: 100, height: 100)
-                                    }
+                if !files.isEmpty {
+                    ScrollView {
+                        LazyVGrid(columns: gridItemLayout, spacing: 0) {
+                            ForEach(files, id: \.pathLower) { file in
+                                if let image = images[file.pathLower ?? ""] {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .onTapGesture {
+                                            self.image = image
+                                            isShowDropboxList = false
+                                            isSelected = true
+                                        }
+                                } else {
+                                    ProgressView()
                                 }
                             }
+                            
                         }
-                        .navigationTitle("Dropbox Image")
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading) {
-                                Button("Cancel") {
-                                    isShowDropboxList = false
-                                }
-                            }
-                        }
-                    } else {
-                        ProgressView()
+                        .padding()
                     }
+                } else {
+                    ProgressView()
                 }
             }
-
+            
+            
+            
             .sheet(isPresented: $viewModel.isShowSheet,onDismiss: {
                 image = nil
                 showingToolbar = true
