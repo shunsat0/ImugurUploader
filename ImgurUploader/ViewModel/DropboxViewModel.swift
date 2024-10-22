f7703a6fc01f1eef146adf147b7db6e9fc1c38b6
shunsat0 <ellegardenno8@gmail.com>
2024-10-22 23:45:50 +0900

    add : ログアウトを追加

--- ImgurUploader/ViewModel/DropboxViewModel.swift
+++ ImgurUploader/ViewModel/DropboxViewModel.swift
@@ -115,5 +115,13 @@ class DropboxViewModel: ObservableObject {
             }
         }
     }
+    
+    func logout() {
+         DropboxClientsManager.unlinkClients()
+         isAuthenticated = false
+         files.removeAll()
+         dropboxImages.removeAll()
+         print("User logged out from Dropbox.")
+     }
 
 }
